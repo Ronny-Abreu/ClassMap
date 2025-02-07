693d2531fda6c528b9c6966c384d0a5832433afd
SlimDReaper <ronnyduran35@gmail.com>
2025-02-07 00:29:00 -0400

    Added "Administrator" class

---
+++ ClassMapProject/ClassMapProject/entities/Administrator.cs
@@ -0,0 +1,18 @@
+﻿public class Administrator
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Role { get; set; }
+
+    public Administrator(string name, int age, string role)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Role = role;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"\nAdministrator: {Name}, Age: {Age}, Role: {Role}");
+    }
+}
