4a555a72a091ddf204a984d4c1d7b1bec469fc4d
SlimDReaper <ronnyduran35@gmail.com>
2025-02-06 22:37:47 -0400

    Added Member class as a base for community "members"
    member will be the basis of student and employee

---
+++ ClassMapProject/ClassMapProject/entities/Member.cs
@@ -0,0 +1,18 @@
+﻿public class Member
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Workshop { get; set; }
+
+    public Member(string name, int age, string workshop)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Workshop = workshop;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Member: {Name}, \nAge: {Age}, \nWorkshop: {Workshop}");
+    }
+}
