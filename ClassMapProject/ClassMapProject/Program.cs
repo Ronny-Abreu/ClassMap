61fa27255c966d180e6760444640d55ee5ac39e5
SlimDReaper <ronnyduran35@gmail.com>
2025-02-06 23:32:40 -0400

    instantiating the "Employee" class
    instantiating the "Member" class and added his parameteres

--- ClassMapProject/ClassMapProject/Program.cs
+++ ClassMapProject/ClassMapProject/Program.cs
@@ -3,12 +3,21 @@
 class Program
 {
     static void Main()
-    {
+    {   
+        //COMMUNITY
         Community community = new Community("ITESA", 1935);
         community.ShowInfo();
 
-        Member member = new Member("Jendry J.", 16, "Programming");
+        //Member
+        Member member = new Member("Jendry A.", 16, "Programming");
         member.ShowInfo();
+
+        //EMPLOYEES
+        Employee employee1 = new Employee("Ronny A.", 20, "Software Developer");
+        Employee employee2 = new Employee("Midu D.", 33, "Project Manager");
+
+        employee1.ShowInfo();
+        employee2.ShowInfo();
     }
 
 
