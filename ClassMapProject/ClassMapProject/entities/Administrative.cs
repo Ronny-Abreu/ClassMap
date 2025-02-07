b7ea0bcd587a1a41a2af726c01e4f225cd2f7f6d
SlimDReaper <ronnyduran35@gmail.com>
2025-02-07 00:42:22 -0400

    Added "Administrative" class

---
+++ ClassMapProject/ClassMapProject/entities/Administrative.cs
@@ -0,0 +1,18 @@
+﻿public class Administrative
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Department { get; set; }
+
+    public Administrative(string name, int age, string department)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Department = department;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"\nAdministrative: {Name}, Age: {Age}, Department: {Department}");
+    }
+}
