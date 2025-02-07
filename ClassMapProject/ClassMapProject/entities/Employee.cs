e87e499f0b33b5ab02eb81428d08e7a22bbcea97
SlimDReaper <ronnyduran35@gmail.com>
2025-02-06 23:26:34 -0400

    Added "Employee" class
    Adde Employee class with properties and methods

---
+++ ClassMapProject/ClassMapProject/entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿public class Employee
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Position { get; set; }
+
+    public Employee(string name, int age, string position)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Position = position;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Employee: {Name}, Age: {Age}, Position: {Position}");
+    }
+}
