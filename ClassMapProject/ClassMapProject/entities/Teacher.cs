8386ccdadb7788ae32869344e6927a39359ea19d
SlimDReaper <ronnyduran35@gmail.com>
2025-02-07 00:14:53 -0400

    Added "Teacher" class with parameters and method

---
+++ ClassMapProject/ClassMapProject/entities/Teacher.cs
@@ -0,0 +1,18 @@
+﻿public class Teacher
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Subject { get; set; }
+
+    public Teacher(string name, int age, string subject)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Subject = subject;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"\nTeacher: {Name}, Age: {Age}, Subject: {Subject}");
+    }
+}
