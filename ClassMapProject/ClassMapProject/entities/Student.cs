44a8383c859e062932eb3bf02dd6bb5d216ff657
SlimDReaper <ronnyduran35@gmail.com>
2025-02-07 00:09:36 -0400

    Added Student class with properties and ShowInfo method

---
+++ ClassMapProject/ClassMapProject/entities/Student.cs
@@ -0,0 +1,18 @@
+﻿public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Career { get; set; }
+
+    public Student(string name, int age, string career)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Career = career;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"\nStudent: {Name}, Age: {Age}, Career: {Career}");
+    }
+}
