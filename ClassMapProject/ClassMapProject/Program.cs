ffe93e75e049fe7ec8abc9f1defecf436aac94c0
SlimDReaper <ronnyduran35@gmail.com>
2025-02-06 22:23:51 -0400

    instantiating the "community" class
    instantiating the "community" class and  added his parameters

--- ClassMapProject/ClassMapProject/Program.cs
+++ ClassMapProject/ClassMapProject/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Community community = new Community("ITESA", 1935);
+        community.ShowInfo();
+    }
+}
