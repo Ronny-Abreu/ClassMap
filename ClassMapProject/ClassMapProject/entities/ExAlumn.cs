c9a820df39039cab9975a18434597c2bd81dbb7b
SlimDReaper <ronnyduran35@gmail.com>
2025-02-07 00:25:14 -0400

    Added "ExAlumn" class

---
+++ ClassMapProject/ClassMapProject/entities/ExAlumn.cs
@@ -0,0 +1,18 @@
+﻿public class ExAlumn
+{
+    public string Name { get; set; }
+    public int GraduationYear { get; set; }
+    public string Career { get; set; }
+
+    public ExAlumn(string name, int graduationYear, string career)
+    {
+        this.Name = name;
+        this.GraduationYear = graduationYear;
+        this.Career = career;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"\nEx-Alumn: {Name}, graduated in {GraduationYear}, specialized in {Career}");
+    }
+}
