02dd92a3353e56994e1fbcd66a0be00a700a9a15
SlimDReaper <ronnyduran35@gmail.com>
2025-02-06 23:38:04 -0400

    Added line break at the parameters
    Added line break at the parameters, because the print of parameters output went out together

--- ClassMapProject/ClassMapProject/Program.cs
+++ ClassMapProject/ClassMapProject/Program.cs
@@ -13,8 +13,8 @@ class Program
         member.ShowInfo();
 
         //EMPLOYEES
-        Employee employee1 = new Employee("Ronny A.", 20, "Software Developer");
-        Employee employee2 = new Employee("Midu D.", 33, "Project Manager");
+        Employee employee1 = new Employee("\nRonny A.", 20, "Software Developer");
+        Employee employee2 = new Employee("\nMidu D.", 33, "Project Manager");
 
         employee1.ShowInfo();
         employee2.ShowInfo();
